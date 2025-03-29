843e1f36db7704b8b556da6aa8d6a1fdbadae9b8
Ogunyemi  Oluwasemiloore <108068525+Mjay06@users.noreply.github.com>
2025-03-29 19:45:37 +0100

    fixed the nav and image issue

--- component/Header.jsx
+++ component/Header.jsx
@@ -6,14 +6,15 @@ import Link from "next/link";
 
 export default function Header() {
   const pathname = usePathname(); // Get the current route
+  console.log(pathname)
   return (
     <header className="head">
       <img src="/civicLogo.svg" width="100px" alt="Civic Policy Archive Logo" />
       <nav className="navs">
-        <Link href="/" className={pathname === "/" ? "active" : ""}>
+        <Link href="/" className={pathname === "/" ? "active" : "notactive"}>
           Home
         </Link>
-        <Link href="/blog" className={pathname === "/blog" ? "active" : ""}>
+        <Link href="/blog" className={pathname === "/blog" ? "active" : "notactive"}>
           Blog
         </Link>
       </nav>
