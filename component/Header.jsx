7765f22f1556abcd2570322021d7563e5842811f
Ogunyemi  Oluwasemiloore <108068525+Mjay06@users.noreply.github.com>
2025-04-10 17:16:33 +0100

    made changes on the header for the mobile nav

--- component/Header.jsx
+++ component/Header.jsx
@@ -1,23 +1,112 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "./Header.css";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Header() {
   const pathname = usePathname(); // Get the current route
-  console.log(pathname)
+  const [isMobile, setIsMobile] = useState(false); // State to track mobile view
+  console.log(pathname);
   return (
-    <header className="head">
-      <img src="/civicLogo.svg" width="100px" alt="Civic Policy Archive Logo" />
-      <nav className="navs">
-        <Link href="/" className={pathname === "/" ? "active" : "notactive"}>
-          Home
-        </Link>
-        <Link href="/blog" className={pathname === "/blog" ? "active" : "notactive"}>
-          Blog
-        </Link>
-      </nav>
-    </header>
+    <>
+      <header className="head">
+        <img
+          src="/civicLogo.svg"
+          width="100px"
+          alt="Civic Policy Archive Logo"
+        />
+        <nav className="navs">
+          <Link href="/" className={pathname === "/" ? "active" : "notactive"}>
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className={pathname === "/about" ? "active" : "notactive"}
+          >
+            About
+          </Link>
+          <Link
+            href="/podcast"
+            className={pathname === "/podcast" ? "active" : "notactive"}
+          >
+            The Policy Podcast
+          </Link>
+          <Link
+            href="/digest"
+            className={pathname === "/digeest" ? "active" : "notactive"}
+          >
+            Policy Digest
+          </Link>
+          <Link
+            href="/articles"
+            className={pathname === "/articles" ? "active" : "notactive"}
+          >
+            Articles
+          </Link>
+          <Link
+            href="/contact"
+            className={pathname === "/contact" ? "active" : "notactive"}
+          >
+            Contact
+          </Link>
+        </nav>
+        <nav className="mobileNav">
+          {!isMobile && (
+            <img
+              onClick={() => setIsMobile(true)}
+              src="/hamburger.svg"
+              height="30px"
+              alt="Civic Policy Archive Logo"
+            />
+          )}
+          {isMobile && (
+            <img
+              onClick={() => setIsMobile(false)}
+              src="/cross.svg"
+              height="30px"
+              alt="Civic Policy Archive Logo"
+            />
+          )}
+        </nav>
+      </header>
+      {isMobile && (
+        <div className="mobileLinks">
+          <Link href="/" className={pathname === "/" ? "active" : "notactive"}>
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className={pathname === "/about" ? "active" : "notactive"}
+          >
+            About
+          </Link>
+          <Link
+            href="/podcast"
+            className={pathname === "/podcast" ? "active" : "notactive"}
+          >
+            The Policy Podcast
+          </Link>
+          <Link
+            href="/digest"
+            className={pathname === "/digest" ? "active" : "notactive"}
+          >
+            Policy Digest
+          </Link>
+          <Link
+            href="/articles"
+            className={pathname === "/articles" ? "active" : "notactive"}
+          >
+            Articles
+          </Link>
+          <Link
+            href="/contact"
+            className={pathname === "/contact" ? "active" : "notactive"}
+          >
+            Contact
+          </Link>
+        </div>
+      )}
+    </>
   );
 }
