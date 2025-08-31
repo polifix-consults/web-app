6b1ee199c66c70fe01b9895a7ad5793ac6dd87c1
Ogunyemi  Oluwasemiloore <108068525+Mjay06@users.noreply.github.com>
2025-08-31 10:50:13 +0100

    UPDATED BRAYDON PROFILE

--- app/about/page.js
+++ app/about/page.js
@@ -83,11 +83,11 @@ export default function page() {
             </li>
           </ul>
 
-          <h3>TEAM LEAD</h3>
+          <h3>OUR TEAM</h3>
           <div className="teamLead">
             <img src="teamLead.jpg" alt="Team Lead" />
             <div className="teamText">
-              <h3>OLUBUNMI AYANTUNJI</h3>
+              <h3>OLUBUNMI AYANTUNJI(TEAM LEAD)</h3>
               <p>
                 Olubunmi Ayantunji is a Legal Practitioner, Policy Expert, and
                 Government Relations Advisor. He holds a Master’s degree in
@@ -101,20 +101,25 @@ export default function page() {
               </p>
             </div>
           </div>
-             <div className="teamLead">
-            <img src="teamLead.jpg" alt="Team Lead" />
+          <div className="teamLead">
+            <img src="brandon.jpg" alt="Team Lead" />
             <div className="teamText">
-              <h3>OLUBUNMI AYANTUNJI</h3>
+              <h3>BRAYDEN PARK(FINANCIAL CONSULTANT)</h3>
               <p>
-                Olubunmi Ayantunji is a Legal Practitioner, Policy Expert, and
-                Government Relations Advisor. He holds a Master’s degree in
-                Legislative Studies and Policy from the University of Benin
-                Nigeria and a Master of Public Administration (MPA) from the
-                Johnson Shoyama Graduate School of Public Policy(University of
-                Regina). He has has served on the board of the Institute of
-                Public Administratuon of Canada (IPAC) and has almost a decade
-                of experience in the Public service in Nigeria,Ghana and the
-                Provincial Government of Saskatchewan in Canada.
+                Brayden brings a unique mix of financial expertise and
+                people-first thinking to the table. With education in both
+                Psychology and Business from the University of Regina, and
+                nearly a decade of experience managing budgets and strategic
+                planning within the Government of Saskatchewan, he’s well-versed
+                in navigating complex financial systems with clarity and
+                confidence. Now working closely with startups and growing
+                businesses, Brayden focuses on cash flow forecasting, financial
+                modeling, and lean growth strategies. He also owns and operates
+                a successful real estate portfolio, giving him a hands-on
+                understanding of investment and long-term value creation.
+                Whether its building a financial roadmap or helping founders
+                make smart, sustainable decisions—Brayden is all about turning
+                numbers into action.
               </p>
             </div>
           </div>
