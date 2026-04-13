b626566956fc3b0a2a0063746f81f5c997043bcb
Ogunyemi  Oluwasemiloore <108068525+Mjay06@users.noreply.github.com>
2026-04-13 15:44:07 +0100

    fix: update parameter type to promise for article slug in GET request

--- app/api/articles/[slug]/route.ts
+++ app/api/articles/[slug]/route.ts
@@ -3,9 +3,9 @@ import { getArticleBySlug } from "@/app/lib/articles";
 
 export async function GET(
   request: Request,
-  { params }: { params: { slug: string } },
+  { params }: { params: Promise<{ slug: string }> },
 ) {
-  const slug = params.slug;
+  const { slug } = await params;
   const article = await getArticleBySlug(slug);
   if (!article) {
     return NextResponse.json({ error: "Article not found" }, { status: 404 });
